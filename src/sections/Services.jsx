ebbfe3a41c7e328551a75bf4edbdf128b932c78b
Prasad <prasadarote27@gmail.com>
2023-09-03 08:51:41 +0530

    Nike Landing Page

---
+++ src/sections/Services.jsx
@@ -0,0 +1,15 @@
+import { services } from "../constants"
+import ServiceCard from "../Components/ServiceCard"
+const Services = () => {
+  return (
+    <div>
+      <section className="max-container flex justify-center flex-wrap gap-9">
+          {services.map((service)=>(
+            <ServiceCard key={service.label} {...service} />
+          ))}
+      </section>
+    </div>
+  )
+}
+
+export default Services
---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+
+export {
+    Hero,PopularProducts,SuperQuality,Footer,CustomerReviews,
+    SpecialOffer,Services,Subscribe
+}
